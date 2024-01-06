38ad20bf6ea3a9334861790da8faa98f363768ff
Twable <tusharwable10@gmail.com>
2024-01-06 20:33:50 +0530

    Second commit of python

--- sample.py
+++ sample.py
@@ -1,2 +1,23 @@
-print("Hello Git")
+i=10
+print("Variable Type is {} and value is {}",type(i),i)
+print("\n")
 
+f=10.1
+print("Variable type is {} and value is {}",type(f),f)
+print("\n")
+
+name="Tushar Wable"
+print("Variable type is {} and value is {}",type(name),name)
+print("\n")
+
+cities=["Chalisgaon", "Pune"]
+print("Variable type is {} and value is {}",type(cities),cities)
+print("\n")
+
+companys=("TCS","Infosis","Wipro")
+print("Variable type is {} and value is {}", type(companys),companys)
+print("\n")
+
+citycol={"Pune":"Indiara", "Chalisgaon":"RAVI"}
+print("Variable type is {} and value is {}", type(citycol),citycol)
+print("\n")
